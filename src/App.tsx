408d6b255a2f453b0bfd0d2301e3465c3ec4a388
RedThunder1 <redthunderspartan@gmail.com>
2025-11-10 09:32:33 -0600

    Simple Home page added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
+import {Route, Routes} from "react-router-dom";
+import Home from './Components/Home/Home'
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+      <main>
+          <Navbar/>
+          <Routes>
+              <Route index element={<Home/>} />
+          </Routes>
+      </main>
   );
 }
 
---
+++ src/Components/Home/Home.tsx
@@ -0,0 +1,11 @@
+import './Home.css'
+export default function Home() {
+    return (
+        <div className="home">
+            <h1 className="home-title">Orderly</h1>
+            <h3>Orderly is an online ordering service.</h3>
+
+            <div className="browse_button">Browse Stores</div>
+        </div>
+    )
+}
