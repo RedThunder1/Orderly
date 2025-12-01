11456d9f5a373fe5306596ce439a2c9234d05a49
James <redthunderspartan@gmail.com>
2025-12-01 13:55:29 -0600

    Filter in shops page maybe works.

--- src/Components/Shops/Shops.tsx
+++ src/Components/Shops/Shops.tsx
@@ -3,6 +3,7 @@ import Cookies from "universal-cookie";
 import axios from "axios";
 import {useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
+
 /*TODO:
     Open shop page:
         Create shop page.
@@ -14,20 +15,39 @@ export default function Shops() {
 
     let nav = useNavigate()
     const [stores, setStores] = useState([]);
+    const [activeStores, setActiveStores] = useState([]);
 
     async function renderShops() {
         //Send request to backend for stores.
         const cookies = new Cookies()
-        const response = await axios.post('/api/stores/', {
-                message: 'Load All Stores'},
-            {withCredentials: true,
-                headers: {"X-CSRFToken": cookies.get("csrftoken")}})
-            .then(response => {
-                //Load stores
-                let data = response.data
-                setStores(data)
-            })
+        try {
+            const response = await axios.post('/api/stores/', {
+                    message: 'Load All Stores'},
+                {withCredentials: true,
+                    headers: {"X-CSRFToken": cookies.get("csrftoken")}})
+                .then(response => {
+                    //Load stores
+                    let data = response.data
+                    setStores(data)
+                })
+        } catch (error) {
+            //Cant reach backend
+        }
+    }
 
+    function handleFilter() { // Unsure if this works as I cant test it yet
+        const filter = document.getElementById("shops_searchbar") as HTMLInputElement;
+        if (filter.value.length === 0) {
+            setActiveStores(stores)
+        } else {
+            let newItems: [] = [];
+            stores.forEach((item) => {
+                if ((item['name'] as String).includes(filter.value)) {
+                    newItems.push(item);
+                }
+            })
+            setActiveStores(newItems);
+        }
     }
 
     useEffect(() => {
@@ -37,20 +57,20 @@ export default function Shops() {
     return (
         <div className="shops">
             <div className="shops_options">
-                <input className="shops_searchbar" type="text" placeholder="Search" />
-                <div className="shops_filter">Filter</div>
+                <input className="shops_searchbar" id="shops_searchbar" type="text" placeholder="Search" />
+                <div className="shops_filter" onClick={handleFilter}>Filter</div>
             </div>
 
             <h2>Featured Shops</h2>
             <div className="shops_container" id="shops_container">
-                {stores.map((store, index) => (
+                {stores.length > 0 ? stores.map((store) => (
                     <div className="shops_item" onClick={() => {nav('/shop', {state: {store: store}})}}>
                         <div className="shops_item_info">
                             <h3>{store['Name']}</h3>
                             <h4>Distance/rating or some other info here</h4>
                         </div>
                     </div>
-                ))}
+                )): <h2>There are no stores available right now! Please try reloading or trying again later!</h2>}
             </div>
         </div>
     )
